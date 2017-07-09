7d501535d531ff4b5df043aa8f12f863d1502bb5
Manjil Shrestha <manshrestha@growbydata.com>
2017-07-09 22:15:21 +0545

    Vue routers

---
+++ src/components/Foo.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+
+        <router-link to="/bar"> >>>> </router-link> &nbsp; &nbsp;
+        <router-link to="/"> <<<< </router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'foo',
+        data () {
+            return {
+                msg: 'Welcome to Your Foo.js App'
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    h1, h2 {
+      font-weight: normal;
+    }
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+
+        <router-link to="/foo"> >>>> </router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'hello',
+        data () {
+            return {
+                msg: 'Welcome to Your Vue.js App'
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    h1, h2 {
+      font-weight: normal;
+    }
+</style>
